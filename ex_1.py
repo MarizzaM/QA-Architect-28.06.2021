adc6be41a8002591ba967ae75d651dfaf8b1a685
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-06-28 19:31:26 +0300

    Create ex_1.py

---
+++ ex_1.py
@@ -0,0 +1,8 @@
+n1 = int(input('PLease enter number 1: '))
+n2 = int(input('PLease enter number 2: '))
+n3 = int(input('PLease enter number 3: '))
+
+print(f'avg = {(n1 + n2 + n3)/3}');
+
+name = input('Please enter your name:' )
+print("Hello " + name + "!")
